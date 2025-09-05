c4004806f910d26b752941b4d7cb76da92adc80b
Meltoz <meltoz@outlook.fr>
2025-09-05 23:20:26 +0200

    feat(MTIX-6): implement recommendation query/vue

--- pages/video/[slug].vue
+++ pages/video/[slug].vue
@@ -1,30 +1,25 @@
 <script setup lang="ts">
-
 import { useDetailVideo } from '~/composables/video.query';
-import {useQueryClient} from '@tanstack/vue-query';
 
 const route = useRoute();
 
 const slug = computed(() => route.params.slug);
 const pageSize = 10;
-const page = ref(0);
-const search = ref("");
 
 const {video} = useDetailVideo(slug);
-const {allVideos, allVideoRefrech} = useAllVideos(page, pageSize, search);
-
-onMounted(() => {
-  allVideoRefrech();
-})
+const {recommendations,recommendationsNextPage ,isRecommendationNextPage} = useGetRecommandation(slug,  pageSize);
 </script>
 
 <template>
 <h1>{{video?.title}}</h1>
-  <div class="grid grid-cols-5 gap-x-10 gap-y-5 mx-20">
-    <video-card v-for="video in allVideos?.videos"
+  <div
+    v-if="recommendations.length > 0"
+    class="grid grid-cols-5 gap-x-10 gap-y-5 mx-20">
+    <video-card v-for="video in recommendations"
                 :video="video"
                 size="s"
     />
+    <button v-if="isRecommendationNextPage" @click="recommendationsNextPage">Voir plus</button>
   </div>
 </template>
 
