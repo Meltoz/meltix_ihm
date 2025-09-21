3ac83857c7359d2d861af6b1a8027e1bfbfc3eae
Meltoz <meltoz@outlook.fr>
2025-09-21 23:56:33 +0200

    feat(mtix-48):fix header

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -10,7 +10,7 @@ const {mobileLinks} = useHeader();
   <div class=" px-2 my-4 flex justify-between items-center gap-3">
     <h1 class="text-blue-300 text-5xl md:text-6xl xl:text-7xl cursor-pointer font-poppins italic font-bold" @click="navigateTo('/')">Meltix</h1>
     <Search />
-    <UPopover mode="click" :open-delay="100" :close-delay="200" class="block lg:hidden">
+    <UPopover mode="click" :open-delay="100" :close-delay="200" class="block md:hidden">
       <UButton><Menu /></UButton>
       <template #content>
         <UNavigationMenu :items="mobileLinks" orientation="vertical" :ui="{item:'text-7xl', label:'text-7xl', link:'text-2xl'}"/>
